7af15292920294b30fd743605f46f440ada118da
MaxHalford <maxhalford25@gmail.com>
2014-12-31 11:55:09 +0100

    First pdf file

--- Problem 1 - Multiples of 3 and 5.py
+++ Problem 1 - Multiples of 3 and 5.py
@@ -1,5 +1,2 @@
-answer=0                  #define answer
-for i in range(1,1000):   #for 1 to 999
-    if i%3==0 or i%5==0:  #is the integer a multiple of 3 or 5?
-        answer+=i         #if so, add those integers to answer
-print(answer)
+answer=[i for i in range(1,1000) if i%3==0 or i%5==0]
+print (sum(answer))
