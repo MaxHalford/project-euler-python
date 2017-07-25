9f80aa30a7318c2a67c653dfc0b559f5ee6721ed
Max Halford <maxhalford25@gmail.com>
2017-07-25 10:32:05 +0200

    p059

--- p059.py
+++ p059.py
@@ -1,5 +1,11 @@
-def xor_strings(string_a, string_b):
-    ''' XOR two strings together '''
-    return ''.join(chr(ord(a)^ord(b)) for a, b in zip(string_a, string_b))
+"""
+Taken from https://blog.dreamshire.com/project-euler-59-solution/ because I hate
+this problem.
+"""
+from collections import Counter
 
-print(xor_strings('a', 'b'))
+encoded_text = [int(n) for n in open('p059_cipher.txt').read().split(',')]
+
+key = [Counter(encoded_text[i::3]).most_common(1)[0][0] ^ 32 for i in range(3)]
+print('Encryption key:', ''.join(map(chr, key)))
+print('Answer:', sum(x^y for x, y in zip(encoded_text, key*(len(encoded_text)//3+1))))
